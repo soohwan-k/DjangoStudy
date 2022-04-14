5c97606b801de09ee0974900410ec92b2def5fe9
SooHwan Kim <newpoo@naver.com>
2022-04-14 19:29:46 +0900

    첨부파일 다운로드, 파일 이름

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 
@@ -17,3 +19,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
